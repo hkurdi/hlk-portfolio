e7da4f8c742c980c17b020cf49c2dd46e4ad36e9
hkurdi <hamzakurdi5@gmail.com>
2024-06-09 19:10:51 -0400

    Fixed mobile responsiveness

--- FE/src/pages/MobilePage.jsx
+++ FE/src/pages/MobilePage.jsx
@@ -27,14 +27,15 @@ const MobilePage = () => {
   return (
     <div className="flex flex-col bg-gray-950 w-screen h-screen overflow-hidden select-none">
       <div className="flex flex-col items-center justify-center text-center w-full h-full p-4">
-        <h1 className="font-sans  text-white"> hamza luay kurdi.</h1>{" "}
-        <span className="font-thin text-gray-300 text-xs tracking-wide">
-          software engineer, entrepreneur, & multidisciplinary creative.
+        <h1 className="font-sans text-white text-2xl md:text-3xl">
+          Hamza Luay Kurdi
+        </h1>
+        <span className="font-thin text-gray-300 text-xs tracking-wide mb-4 md:text-sm">
+          Software engineer, entrepreneur, & multidisciplinary creative.
         </span>
-        <br />
-        <h1 className="font-bold font-sans text-pretty text-white mb-4">
+        <h1 className="font-bold font-sans text-pretty text-white mb-4 text-lg md:text-xl">
           Note:{" "}
-          <span className="font-normal">
+          <span className="font-normal text-base md:text-lg">
             You are using a small screen / mobile, and my website requires a
             large screen to really enjoy it to the maximum, thus I limited
             mobile to either using it with 50% zoom on your respected browser,
@@ -46,7 +47,7 @@ const MobilePage = () => {
         <HLKButton
           id="resumebutton-mobile"
           text={"Resume"}
-          style={"mt-2 w-40 h-10 rounded-full w-full"}
+          style={"mt-2 w-40 h-10 rounded-full"}
           onClick={handleResumeClick}
         />
       </div>
